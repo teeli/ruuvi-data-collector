7c6bfc696dce582e2c1e7c324723b9367d3b483f
Teemu Lahti <teemu.lahti@gmail.com>
2026-03-07 15:30:50 +0200

    refactor(index): simplify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import { scanner } from '@scanner/scanner'
-import { handleEvent } from '@writers/influxdb-writer'
+import * as influxDbWriter from '@writers/influxdb-writer'
 
-const main = async (): Promise<void> => {
-  await scanner({
-    onEvent: (event) => {
-      handleEvent(event)
-    },
-  })
-}
-
-void main()
+void scanner({ onEvent: influxDbWriter.handleEvent })

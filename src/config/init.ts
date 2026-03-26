e548bfc4c7553bf6c3a4068ed12ab199169e2e9f
Teemu Lahti <teemu.lahti@gmail.com>
2026-03-26 20:02:25 +0200

    fix: fix lint errors

--- src/config/init.ts
+++ src/config/init.ts
@@ -1,6 +1,6 @@
 import config from '../../config'
 import { setConfig } from '@config/config'
-import { configureLogger } from '@config/logger/logger.ts'
+import { configureLogger } from '@config/logger/logger'
 
 setConfig(config)
 await configureLogger(config)
